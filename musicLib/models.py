e08328e91ec2c8cd11c607aa30389bdf88ca6f81
Anil Poudyal <anilrunjakote@gmail.com>
2020-01-07 16:39:54 +0545

    Added musicLib App with models

--- musicLib/models.py
+++ musicLib/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime
+from PIL import image
 
-# Create your models here.
+def music(models.Model):
+    music_title = models.CharField(max_length=200)
+    music_artist= models.CharField(max_length=200)
+    music_length = models.CharField(max_length=200)
+    music_album= models.CharField(max_length=200)
+    music_coverArt= models.ImageField(upload_to='Cover_Art')
+    date_added= datetime.datetime.now()
+
+def contact(models.Model):
+    contact_title = models.CharField(max_length=200)
+    contact_email = models.EmailField()
+    contact_phone = models.IntegerField()
+    contact_message = models.TextField()
+
+def customer(models.Models):
+    f_name
+    m_name
+    l_name
+    dob
+    phone
+    email
+    gender
+    
+    
+    
