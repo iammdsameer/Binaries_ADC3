d3dfd25f0460925087867278423964b8c2de7061
Anil Poudyal <anilrunjakote@gmail.com>
2020-01-08 12:36:20 +0545

    Created Models and added base template

--- musicLib/models.py
+++ musicLib/models.py
@@ -1,29 +1,28 @@
 from django.db import models
 import datetime
-from PIL import image
+#from PIL import image
 
-def music(models.Model):
+
+class music(models.Model):
     music_title = models.CharField(max_length=200)
-    music_artist= models.CharField(max_length=200)
+    music_artist = models.CharField(max_length=200)
     music_length = models.CharField(max_length=200)
-    music_album= models.CharField(max_length=200)
-    music_coverArt= models.ImageField(upload_to='Cover_Art')
-    date_added= datetime.datetime.now()
+    music_album = models.CharField(max_length=200)
+   # music_coverArt = models.ImageField(upload_to='Cover_Art')
+    date_added = datetime.datetime.now()
 
-def contact(models.Model):
+
+class contact(models.Model):
     contact_title = models.CharField(max_length=200)
     contact_email = models.EmailField()
     contact_phone = models.IntegerField()
     contact_message = models.TextField()
 
-def customer(models.Models):
-    f_name
-    m_name
-    l_name
-    dob
-    phone
-    email
-    gender
-    
-    
-    
+# def customer(models.Models):
+#     f_name
+#     m_name
+#     l_name
+#     dob
+#     phone
+#     email
+#     gender
