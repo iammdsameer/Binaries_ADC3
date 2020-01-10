6b104370e846692c88998d43792c78652a7ac742
Anil Poudyal <anilrunjakote@gmail.com>
2020-01-10 12:25:54 +0545

    Fixed some bugs

--- user/views.py
+++ user/views.py
@@ -10,7 +10,7 @@ def register(request):
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        user = user.objects.create_user(username=username, password=password1,email=email,first_name=first_name,last_name=last_name)
+        user = User.objects.create_user(username=username, password=password1,email=email,first_name=first_name,last_name=last_name)
         user.save()
         return redirect('/')
     else:
