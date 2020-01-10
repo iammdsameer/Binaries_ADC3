dc68d7a1e0301a0d459ce1102db9225c9adc8b41
Anil Poudyal <anilrunjakote@gmail.com>
2020-01-10 22:07:41 +0545

    Added Music App

---
+++ music/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def music(request):
+    return render(request,'music/music.html')
+
+def playMusic(request):
+    
+    pass
+
+def pauseMusic(request):
+    pass
+
+def shuffalMusic(request):
+    pass
+
+def changeMusic(request):
+    #next music
+    #prev music
+    pass
