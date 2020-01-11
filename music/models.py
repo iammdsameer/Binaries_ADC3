b08b76c3a9fb4497ed2c4de973bafd8f1090fbbb
Anil Poudyal <anilrunjakote@gmail.com>
2020-01-11 12:31:31 +0545

    Added Music Model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.FileField()
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Music(models.Model):
+    # How to use
+     #music_album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    music_length = models.FloatField(null=True)
+    music_title = models.CharField(max_length=250)
+    music_file = models.FileField(default='')
+    music_coverArt = models.CharField(max_length=2083, default='https://bit.ly/2FDO3LI')
+    #music_coverArt = models.ImageField(upload_to='media/')
+    music_file = models.FileField(upload_to='musics/', blank =True)
+
+    def __str__(self):
+        return self.music_title
--- music/views.py
+++ music/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from .models import Music
+from django.http import HttpResponse
+
 
 
 def music(request):
-    return render(request,'music/music.html')
+    musics = Music.objects.all()
+    return render(request,'music/music.html',{'music':musics})
 
 def upload(request):
-    return render(request,'music/upload.html')
+    if request.method == 'POST':
+        music_title = request.POST['music_title']
+        music_length = request.POST['music_length']
+        #music_album = request.POST['music_album']
+        music_coverArt = request.POST['music_coverArt']
+        music_file = request.POST['music_file']
+        upload = Music(music_title=music_title, music_coverArt=music_coverArt, music_file=music_file, music_length=music_length)
+        upload.save()
+        return HttpResponse('File Uploaded')
+    else:   
+        return render(request,'music/upload.html')
 
 def playMusic(request):
     
