c8ecd8d31f6674e7d49eb121b790e36e6d786b2c
Anil Poudyal <anilrunjakote@gmail.com>
2020-01-11 18:19:50 +0545

    Commit to master

--- musicLib/views.py
+++ musicLib/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
+    if request.method == 'POST':
+        search_tag = request.POST['search_tag']
+      
     return render(request, 'musicLib/index.html', context={})
 
 
