df7679437bf2ee2b6b4bef2da716790ec8b65e0c
Sameer <itsmdsameerkhan@gmail.com>
2020-01-11 11:26:37 +0545

    File Upload Function

--- music/models.py
+++ music/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.FileField()
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Music(models.Model):
+    # How to use
+    # album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    music_title = models.CharField(max_length=250)
+    music_file = models.FileField(default='')
+    music_coverArt = models.ImageField(upload_to='media/')
+
+    def __str__(self):
+        return self.music_title
