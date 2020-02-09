b5b13dc717c3dc40e5ce00b79c79145250464229
Anil Poudyal <anilrunjakote@gmail.com>
2020-02-09 16:01:53 +0545

    REST API Add Music, Delete Music

--- restApi/views.py
+++ restApi/views.py
@@ -15,15 +15,33 @@ def edit(request, pk):
     if request.method == "GET":
         return JsonResponse({"musics": music.music_title, "length": music.music_length})
 
-    else:
+    elif request.method =="PUT":
         json_data = request.body.decode('utf-8')
         update_data = json.loads(json_data)
-        music.music_title = update_data['title']
-        music.music_length = update_data['length']
+        music.music_title = update_data['music_title']
+        music.music_length = update_data['music_length']
         music.save()
-        return JsonResponse({'message': 'Upadated Successfully!!'})
-    
-
+        return JsonResponse({'Success': 'Upadated Successfully!!'})
+    elif request.method =="DELETE":
+        music.delete()
+        return JsonResponse({'Success': 'Music Deleted'})
+        
+@csrf_exempt    
+def addMusic(request):
+    if request.method =='POST':
+        json_data = request.body.decode('utf-8')
+        new = json.loads(json_data)
+        music_title = new['title']
+        length = new['length']
+        music = Musics.objects.create(music_title=music_title,music_length=length)
+        try:
+            music.save()
+            return JsonResponse({"Success":"New music added successfully!!"})
+        except:
+            return JsonResponse({"Error":"Music could not be added"})
+        
+        
+        
 def music_pagination(request,PAGENO,SIZE):
     skip = SIZE*(PAGENO-1)
     music = Musics.objects.all()[skip:(PAGENO*SIZE)]
